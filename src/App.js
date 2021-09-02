9e8fabf5e7665dbe2d95624f9e5e705a542bcddc
Marcio B Cyrilo <m.bruno.cs@hotmail.com>
2021-09-02 17:20:56 -0300

    Iniciando projeto Trivia - Grupo 7

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import logo from './trivia.png';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import Game from './pages/Game';
+import Feedback from './pages/Feedback';
+import Ranking from './pages/Ranking';
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          SUA VEZ
-        </p>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path="/ranking" component={ Ranking }/>
+      <Route exact path="/feedback" component={ Feedback }/>
+      <Route exact path="/game" component={ Game }/>
+      <Route exact path="/" component={ Login }/>
+    </Switch>
   );
 }
