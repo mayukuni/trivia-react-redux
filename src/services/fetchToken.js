26330e20a543660c8d7a38cbe7bc7d39558cb52b
Alex <alexcorreas1994@gmail.com>
2021-09-03 15:25:01 -0300

    .

--- src/services/fetchToken.js
+++ src/services/fetchToken.js
@@ -1,16 +1,11 @@
-// import getToken from '../redux/actions';
-
 const apiToken = 'https://opentdb.com/api_token.php?command=request';
 
 const fetchToken = async () => {
   const response = await fetch(apiToken);
   const data = await response.json();
   const { token } = data;
-  console.log(token);
-  // const player = JSON.parse(localStorage.getItem('player'));
-  // localStorage.setItem('player', JSON.stringify({ ...player, name }));
-  // localStorage.setItem('token', JSON.stringify(token));
-  // dispatch(getToken({ token, name, email }));
+  // console.log(token);
+  localStorage.setItem('token', token);
 };
 
 export default fetchToken;
