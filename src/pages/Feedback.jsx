94e67240a04302d5139c9e12fd5c6100e97e33fa
Marcio B Cyrilo <m.bruno.cs@hotmail.com>
2021-09-08 15:39:06 -0300

    Criado componente FeedbackHeader

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import FeedbackMsg from '../components/FeedbackMsg';
+// import PropTypes from 'prop-types';
+import FeedbackHeader from '../components/FeedbackHeader';
 
 class Feedback extends Component {
+  // const { name, image, score } = this.props;
   render() {
-    const { name, image, score } = this.props;
     return (
       <div>
-        <header>
-          <img src={ image } alt="User logo" data-testid="header-profile-picture" />
-          <h2 data-testid="header-player-name">{ name }</h2>
-          <span data-testid="header-score">{ score }</span>
-        </header>
-        <FeedbackMsg />
+        <FeedbackHeader />
+        Feedback
       </div>
     );
   }
 }
 
-Feedback.propTypes = {
+/* Feedback.propTypes = {
   name: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
   score: PropTypes.number.isRequired,
@@ -29,6 +25,6 @@ const mapStateToProps = (state) => ({
   name: state.reducer.name,
   image: state.reducer.image,
   score: state.reducer.score,
-});
+}); */
 
 export default connect(mapStateToProps)(Feedback);
